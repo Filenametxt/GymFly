bf39d15b39c7e3a1b9e809a64dde3efa0b630286
Mattia Di Rico <mattiadr200@gmail.com>
2026-05-26 11:37:28 +0200

    feat(core): aggiunta classi Abbonamento e AbbonamentoMensile

---
+++ src/Entity/Abbonamento.php
@@ -0,0 +1,51 @@
+<?php
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\Collection;
+
+class Abbonamento{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    private int $data_inizio;
+    private string $tipologia;
+    private Collection $cliente;
+
+    public function __construct(int $data_inizio, string $tipologia){
+        $this->data_inizio = $data_inizio;
+        $this->tipologia = $tipologia;
+    }
+
+    public function getId(){
+        return $this->id;
+    }
+
+    public function getData_inizio(){
+        return $this->data_inizio;
+    }
+
+    public function getTipologia(){
+        return $this->tipologia;
+    }
+
+    public function getCliente(){
+        return $this->cliente;
+    }
+
+    public function setData_inizio(int $data_inizio):self{
+        $this->data_inizio = $data_inizio;   
+        return $this;                   
+    }
+    public function setTipologia(string $tipologia):self{
+        $this->tipologia = $tipologia;
+        return $this;
+    }
+    public function setCliente(Collection $cliente):self{
+        $this->cliente = $cliente;
+        return $this;
+    }
+
+}
+
+?>
---
+++ src/Entity/AbbonamentoMensile.php
@@ -0,0 +1,21 @@
+<?php
+use Doctrine\ORM\Mapping as ORM;
+
+class AbbonamentoMensile extends Abbonamento{
+
+    private int $durata; //REVIEW: revisione per la durata
+    
+    public function __construct(int $data_inizio, string $tipologia, int $durata){
+        parent::__construct($data_inizio, $tipologia);
+        $this->durata = $durata;
+    }
+
+    public function getDurata(){
+        return $this->durata;
+    }
+    public function setDurata(int $durata):self{
+        $this->durata = $durata;
+        return $this;
+    }    
+}
+?>
